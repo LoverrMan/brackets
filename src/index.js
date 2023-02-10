96a43a42d1051d77e265d52aca6c4fb5a5968a04
LoverrMan <livetskyi.oleksandr@gmail.com>
2023-02-10 09:31:15 +0100

    fix: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let result = brackConf(str, bracketsConfig);
+  function brackConf(str, bracketsConfig) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+     const a = bracketsConfig[i][0]
+     const b = bracketsConfig[i][1]
+      if (str.includes(`${a}${b}`)) {str = str.replace(`${a}${b}`, ``);
+        return brackConf(str, bracketsConfig);}}
+    return str;
+  }
+  return result.length === 0;
+};
